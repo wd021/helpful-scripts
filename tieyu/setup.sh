f59af771edfb6bc4ce38bec2047578c59a428c8b
wd021 <dannywitters@gmail.com>
2022-08-27 13:40:50 +0200

    更新 - make script run unattended

--- tieyu/setup.sh
+++ tieyu/setup.sh
@@ -13,12 +13,12 @@ echo -e '\n\e[42mInstalling dependencies...\e[0m\n' && sleep 1
 cd $HOME
 sudo apt-get update -y
 sudo apt update
-sudo apt upgrade
+sudo apt upgrade -y
 sudo apt-get install -y build-essential
 sudo apt install -y curl
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt install -y nodejs
-curl — proto ‘=https’ — tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl — proto ‘=https’ — tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 sudo npm install -global yarn
 
