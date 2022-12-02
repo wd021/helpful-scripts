049835835d92431389d7dfe8ead8e4bfa6307f87
wd021 <dannywitters@gmail.com>
2022-12-02 06:57:24 +0100

    回到basics

--- ailio/prover.sh
+++ ailio/prover.sh
@@ -2,19 +2,7 @@
 
 curl -s https://raw.githubusercontent.com/wd021/helpful-scripts/main/wd021.sh | bash
 
-if [[ "$1" != "" ]]; then
-    privateKey="$1"
-else
-    echo -e "What's your aleo prover pk?"
-    read privateKey
-fi
-
 cd $HOME
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd snarkOS
 ./build_ubuntu.sh
-export PROVER_PRIVATE_KEY=$privateKey
-sleep 5
-cd $HOME
-cd snarkOS
-screen -S proving -dm ./run-prover.sh
