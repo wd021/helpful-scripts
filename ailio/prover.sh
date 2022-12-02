3211c3a208cac522bf0a2d337ab8f4e8158f1350
wd021 <dannywitters@gmail.com>
2022-12-02 01:07:53 +0100

    更新

--- ailio/prover.sh
+++ ailio/prover.sh
@@ -10,17 +10,8 @@ else
 fi
 
 cd $HOME
-sudo apt-get update -y
-sudo apt update
-sudo apt upgrade -y
-sudo apt-get install -y build-essential
-sudo apt install -y curl
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
-sudo apt install -y nodejs
-curl — proto ‘=https’ — tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
 git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 cd snarkOS
-source ./build_ubuntu.sh
-cargo install --path .
-screen -S proving -dm source PROVER_PRIVATE_KEY=$privateKey ./run-prover.sh
+./build_ubuntu.sh
+export PROVER_PRIVATE_KEY=$privateKey
+screen -S proving -dm ./run-prover.sh
