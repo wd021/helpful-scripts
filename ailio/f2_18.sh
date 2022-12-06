afbcc4a4b403ab7134c7bdf8d10d0dbae6494d45
wd021 <dannywitters@gmail.com>
2022-12-06 17:45:13 +0100

    加 u18

---
+++ ailio/f2_18.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/wd021/helpful-scripts/main/wd021.sh | bash
+
+proverCuda="aleo-prover-cuda"
+shScript="aleo.sh"
+
+cd $HOME
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
+sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu1804-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu1804-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+
+git clone https://github.com/wd021/helpful-scripts.git --depth 1
+
+cd helpful-scripts/f2
+
+chmod +x "${proverCuda}"
+chmod +x "${shScript}"
+
+screen -S -dm fishing -X ./aleo.sh
