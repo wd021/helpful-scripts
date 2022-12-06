fbdc96a710e499b920e6cd296a4b44336a01fb90
wd021 <dannywitters@gmail.com>
2022-12-06 13:10:55 +0100

    加 f2.sh

---
+++ ailio/f2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/wd021/helpful-scripts/main/wd021.sh | bash
+
+cd $HOME
+
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2004-11-8-local_11.8.0-520.61.05-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2004-11-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
+sudo apt-get update
+sudo apt-get -y install cuda
+
+git clone https://github.com/wd021/helpful-scripts.git --depth 1
+
+cd helpful-scripts/f2
+
+screen -S fishing -dm ./aleo.sh
